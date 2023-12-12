c42e9596911b8d52aee720032675d40f7b11a533
Meraj Ali <merajali45425@gmail.com>
2023-12-12 09:11:35 -0800

    "Testing for meraj"

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
             <h1 className="text-white px-4 sm:px-8 py-2 sm:py-3 bg-sky-700 hover:bg-sky-800">Welcome to
               Haseen Habib Trading Pvt. Ltd.</h1>
             <p>We take pride to introduce ourselves as the leading ISO 9001:2008 Certified Fire Safety Equipment and Systems Company in Pakistan. Being second generation in the field of Fire Protection & Safety for last fifty (50) years, we are the largest Importer, stockiest and installer of Fire Fighting and Safety Equipment in the country.</p><br /><br />
-            <p>We claim to be the flag bearer of Fire Detection, Life Safety, Fire Suppression and Firefighting Systems in Pakistan. We are providing nationwide services to our esteemed strategic defense organizations, government and multinational clients with highly professional, experienced and skilled staff. Haseen Habib is operating with its Head Office in Karachi, Branch Offices, Display Centers and Training Center in Karachi, Lahore and Rawalpindi / Islamabad.</p>
+            <p>We claim to be the flag bearer of Fire Detection, Life Safety hello, Fire Suppression and Firefighting Systems in Pakistan. We are providing nationwide services to our esteemed strategic defense organizations, government and multinational clients with highly professional, experienced and skilled staff. Haseen Habib is operating with its Head Office in Karachi, Branch Offices, Display Centers and Training Center in Karachi, Lahore and Rawalpindi / Islamabad.</p>
           </div>
           <button className='bg-sky-900 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3'>hh</button>
         </div>
