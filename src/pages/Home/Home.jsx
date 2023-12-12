cf5704c923176be48519383352b0f3a572b27799
Meraj Ali <merajali45425@gmail.com>
2023-12-12 09:08:29 -0800

    "Tailwind Connected"

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
-import Navbar from '../../components/navbar/Navbar'
+
+import '../../App.css'
+// import '../../css Files/home.css'
 const Home = () => {
   return (
     <>
-    <div>Home</div>
+      <div className='mainContainer'>
+        <div className="wrapper">
+          <div>
+            <img src="https://prod.haseenhabib.com/web/image/10412-56bfe3d9/R%20%283%29.jpg" alt="not found" />
+          </div>
+          <div>
+            <h1 className="text-white px-4 sm:px-8 py-2 sm:py-3 bg-sky-700 hover:bg-sky-800">Welcome to
+              Haseen Habib Trading Pvt. Ltd.</h1>
+            <p>We take pride to introduce ourselves as the leading ISO 9001:2008 Certified Fire Safety Equipment and Systems Company in Pakistan. Being second generation in the field of Fire Protection & Safety for last fifty (50) years, we are the largest Importer, stockiest and installer of Fire Fighting and Safety Equipment in the country.</p><br /><br />
+            <p>We claim to be the flag bearer of Fire Detection, Life Safety, Fire Suppression and Firefighting Systems in Pakistan. We are providing nationwide services to our esteemed strategic defense organizations, government and multinational clients with highly professional, experienced and skilled staff. Haseen Habib is operating with its Head Office in Karachi, Branch Offices, Display Centers and Training Center in Karachi, Lahore and Rawalpindi / Islamabad.</p>
+          </div>
+          <button className='bg-sky-900 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3'>hh</button>
+        </div>
+
+      </div>
     </>
   )
 }
